4c6d8e5e1b5bd40442866439f7d5ca6930eed630
Sergio <sergio.villen.galera@hotmail.fr>
2015-12-13 00:24:52 +0100

    Should_return_false_when_year_is_a_typical_leap_year

--- LeapYearKata/LeapYearKata/LeapYearTests.cs
+++ LeapYearKata/LeapYearKata/LeapYearTests.cs
@@ -16,12 +16,19 @@ namespace LeapYearKata
             var actual = LeapYear.IsLeapYear(2001);
             Check.That(actual).IsFalse();
         }
+        [Test]
+        public void Should_return_false_when_year_is_a_typical_leap_year()
+        {
+            var actual = LeapYear.IsLeapYear(1996);
+            Check.That(actual).IsTrue();
+        }
     }
 
     public class LeapYear
     {
         public static bool IsLeapYear(int year)
         {
+            if (year%4 == 0) return true;
             return false;
         }
     }
