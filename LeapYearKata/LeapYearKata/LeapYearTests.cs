b6451fba752efcba2f55e9d5d7f625bf899e4085
Sergio <sergio.villen.galera@hotmail.fr>
2016-02-24 22:01:26 +0100

    Refactoring tests

--- LeapYearKata/LeapYearKata/LeapYearTests.cs
+++ LeapYearKata/LeapYearKata/LeapYearTests.cs
@@ -10,32 +10,14 @@ namespace LeapYearKata
 {
     public class LeapYearTests
     {
-        [Test]
-        public void Should_return_false_when_year_is_a_common_year()
+        [TestCase(2001, false)]
+        [TestCase(1996, true)]
+        [TestCase(1900, false)]
+        [TestCase(2000, true)]
+        public void Should_return_true_when_year_is_leap_year(int year, bool isLeapYear)
         {
-            var actual = LeapYear.IsLeapYear(2001);
-            Check.That(actual).IsFalse();
-        }
-
-        [Test]
-        public void Should_return_true_when_year_is_a_typical_leap_year()
-        {
-            var actual = LeapYear.IsLeapYear(1996);
-            Check.That(actual).IsTrue();
-        }
-
-        [Test]
-        public void Should_return_false_when_year_is_an_atypical_common_year()
-        {
-            var actual = LeapYear.IsLeapYear(1900);
-            Check.That(actual).IsFalse();
-        }
-
-        [Test]
-        public void Should_return_true_when_year_is_an_atypical_leap_year()
-        {
-            var actual = LeapYear.IsLeapYear(2000);
-            Check.That(actual).IsTrue();
+            var actual = LeapYear.IsLeapYear(year);
+            Check.That(actual).IsEqualTo(isLeapYear);
         }
     }
 }
